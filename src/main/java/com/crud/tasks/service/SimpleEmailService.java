dca096bd0675f3a8de2b11f2afc925576341a71f
marcinkostka <marcinkostka@gmail.com>
2018-04-14 11:23:40 +0200

    19.4 EmailScheduler

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -32,7 +32,7 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if (!(mail.getToCC() == null)) mailMessage.setCc(mail.getToCC());
+        if (mail.getToCC() != null) mailMessage.setCc(mail.getToCC());
         return mailMessage;
     }
 }
